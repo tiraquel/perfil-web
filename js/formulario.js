43eb6fbaf666c740fd4521294020e125dc08a68a
Raquel Fonseca <ti.raquel@gmail.com>
2018-07-24 14:41:12 -0300

    trabalhando com formulario e js

---
+++ js/formulario.js
@@ -0,0 +1,10 @@
+function enviar()
+  {var nome = document.getElementById("nome");
+  if(nome.value !=""){
+    alert("Obrigado " +nome.value+ " ! Mas esta área temporáriamente não está funcionando." );
+  } else {
+    alert("Por gentileza, coloque seus dados");
+  }
+}
+// var email = document.getElementById("email");
+// var telefone - document.getElementById
